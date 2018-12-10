95429b45d89c6ef271d59a32bdd72fe33dc66864
allah.rakha <allah.rakha@localhost>
2018-12-10 20:13:28 +0530

    main-server first commit

---
+++ main-server/src/main/java/com/helptree/eureka/MainServerApplication.java
@@ -0,0 +1,16 @@
+package com.helptree.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
+public class MainServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServerApplication.class, args);
+	}
+}
